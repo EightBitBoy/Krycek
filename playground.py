c2705b159c3eeabe5c5c17339193532a4dd4d200
Stefan Härtel <haertel.dev@gmail.com>
2016-01-22 10:11:52 +0100

    Fiddle

--- playground.py
+++ playground.py
@@ -1,1 +1,15 @@
-print("playing")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "index"
+
+@app.route("/status")
+def status():
+	return "status"
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
